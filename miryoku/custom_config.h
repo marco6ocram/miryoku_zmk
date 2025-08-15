65f1b9e33bf7b87a87076ebd08ab60a5c284af36
6ocram <marcosocram@protonmail.com>
2025-08-15 11:02:31 -0600

    Swaped the mouse and sym key directly int eh layer alternatives file

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,15 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_SYM, TAB),  U_LT (U_MOUSE, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
-
-
-#define MIRYOKU_LAYER_MOUSE \
+/*#define MIRYOKU_LAYER_MOUSE \
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_CPY,             U_BTN1,            U_MS_U,            U_BTN2,            U_CUT,            \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_PST,             U_MS_L,            U_MS_D,            U_MS_R,            U_UND,            \
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_WH_L,            U_WH_D,            U_BTN3,            U_WH_U,            U_WH_R,            \
-U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP*/
