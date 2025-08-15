f07b6820eb4b3186fa821d4bdfcc952a112302e3
6ocram <marcosocram@protonmail.com>
2025-08-15 10:34:32 -0600

    Removed Btn 1 from mouse layer left side

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -12,4 +12,4 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_CPY,             U_BTN1,            U_MS_U,            U_BTN2,            U_CUT,            \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_PST,             U_MS_L,            U_MS_D,            U_MS_R,            U_UND,            \
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_WH_L,            U_WH_D,            U_BTN3,            U_WH_U,            U_WH_R,            \
-U_NP,              U_NP,              U_BTN1,        U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
