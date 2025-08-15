8cad496935e1590f22ae3bfa71900b44b674b904
6ocram <marcosocram@protonmail.com>
2025-08-15 17:49:38 -0600

    Changed position of BTN1

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -303,7 +303,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_CPY,             U_BTN1,            U_MS_U,            U_BTN2,            U_CUT,            \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_PST,             U_MS_L,            U_MS_D,            U_MS_R,            U_UND,            \
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_WH_L,            U_WH_D,            U_BTN3,            U_WH_U,            U_WH_R,            \
-U_BTN1,            U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_BTN1,              U_NA,              U_NA,              U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_MOUSE \
 U_BOOT,            &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
