327ed2bffb68de070be7313fea796457e6153178
6ocram <marcosocram@protonmail.com>
2025-08-15 18:08:40 -0600

    Deleted conten of custom_config.h. Added configurations for sleep mode at miryoku.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-/*#define MIRYOKU_LAYER_MOUSE \
-U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_CPY,             U_BTN1,            U_MS_U,            U_BTN2,            U_CUT,            \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              U_PST,             U_MS_L,            U_MS_D,            U_MS_R,            U_UND,            \
-U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_WH_L,            U_WH_D,            U_BTN3,            U_WH_U,            U_WH_R,            \
-U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NP,              U_NP*/
